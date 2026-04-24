c2f3d4c0dc4753b9af6ecc386114479be7eda95b
Mauro <maurogt.dd@gmail.com>
2026-04-24 11:17:09 -0300

    Infolist de registro de modelos

--- app/Filament/Resources/Modelos/ModeloResource.php
+++ app/Filament/Resources/Modelos/ModeloResource.php
@@ -49,85 +49,105 @@ class ModeloResource extends Resource
         return $schema
         ->components([
              ComponentsSection::make('Mercado')
-                    ->columns(3)
+                    ->columns(2)
                     ->schema([
                         Infolists\Components\TextEntry::make('precio_venta_faena')
-                            ->label('Precio venta a faena ($/kg)')
+                            ->label('Precio venta a faena')
                             ->numeric(
                                 decimalPlaces: 1,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' $/Kg')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('precio_compra_ternero')
-                            ->label('Precio compra terneras/os destete ($/kg)')
+                            ->label('Precio compra terneras/os destete')
                             ->numeric(
                                 decimalPlaces: 1,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' $/Kg')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('peso_neto_entrada')
-                            ->label('Peso neto de entrada (kg)')
+                            ->label('Peso neto de entrada')
                             ->numeric(
                                 decimalPlaces: 2,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' Kg')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('peso_neto_venta')
-                            ->label('Peso neto venta (kg)')
+                            ->label('Peso neto venta')
                             ->numeric(
                                 decimalPlaces: 2,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' Kg')
+                            ->size('lg'),
                     ]),
 
                 ComponentsSection::make('Financiero')
-                    ->columns(3)
+                    ->columns(2)
                     ->schema([
                         Infolists\Components\TextEntry::make('tasa_anual')
-                            ->label('Tasa anual (%)')
+                            ->label('Tasa anual')
                             ->numeric(
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix('%')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('plazo_compra_hacienda')
-                            ->label('Plazo compra hacienda (días)')
+                            ->label('Plazo compra hacienda')
                             ->numeric(
                                 decimalPlaces: 0
-                            ),
+                            )
+                            ->suffix(' días')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('plazo_venta_hacienda')
-                            ->label('Plazo venta hacienda (días)')
+                            ->label('Plazo venta hacienda')
                             ->numeric(
                                 decimalPlaces: 0
-                            ),
+                            )
+                            ->suffix(' días')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('dias_financiamiento_alimento')
                             ->label('Días de financiamiento alimento')
                             ->numeric(
                                 decimalPlaces: 0
-                            ),
+                            )
+                            ->suffix(' días')
+                            ->size('lg'),
                     ]),
 
                 ComponentsSection::make('Comercialización')
                     ->columns(2)
                     ->schema([
                         Infolists\Components\TextEntry::make('cabezas_jaula_terneros')
-                            ->label('Cabezas/jaula (Terneros/as)')
+                            ->label('Cabezas/jaula')
                             ->numeric(
                                 decimalPlaces: 0
-                            ),
+                            )
+                            ->suffix(' Terneros/as')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('cabezas_jaula_gordos')
-                            ->label('Cabezas/jaula (Gordos/as)')
+                            ->label('Cabezas/jaula')
                             ->numeric(
                                 decimalPlaces: 0
-                            ),
+                            )
+                            ->suffix(' Gordos/as')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('flete_compra_km')
                             ->label('Flete compra (km)')
@@ -135,39 +155,48 @@ class ModeloResource extends Resource
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('flete_venta_km')
-                            ->label('Flete venta (km)')
+                            ->label('Flete venta')
                             ->numeric(
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' km')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('flete_compra_venta_precio')
-                            ->label('Flete compra-venta - precio ($/km)')
+                            ->label('Flete compra-venta - precio')
                             ->numeric(
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' $/Kg')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('gastos_compra')
-                            ->label('Gastos de compra (%)')
+                            ->label('Gastos de compra')
                             ->numeric(
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' %')
+                            ->size('lg'),
 
                         Infolists\Components\TextEntry::make('gastos_venta')
-                            ->label('Gastos de venta (%)')
+                            ->label('Gastos de venta')
                             ->numeric(
                                 decimalPlaces: 0,
                                 decimalSeparator: ',',
                                 thousandsSeparator: '.'
-                            ),
+                            )
+                            ->suffix(' %')
+                            ->size('lg'),
                     ]),
 
                 ComponentsSection::make('')
@@ -176,40 +205,48 @@ class ModeloResource extends Resource
                         ComponentsSection::make('Sanidad')
                             ->schema([
                                 Infolists\Components\TextEntry::make('mortandad')
-                                    ->label('Mortandad (1%)')
+                                    ->label('Mortandad')
                                     ->numeric(
                                         decimalPlaces: 1,
                                         decimalSeparator: ',',
                                         thousandsSeparator: '.'
-                                    ),
+                                    )
+                                    ->suffix('%')
+                                    ->size('lg'),
                             ]),
 
                         ComponentsSection::make('Nutrición')
-                            ->columns(3)
+                            ->columns(2)
                             ->schema([
                                 Infolists\Components\TextEntry::make('precio_alimento_balanceado')
-                                    ->label('Precio tal cual alimento balanceado ($/kg)')
+                                    ->label('Precio tal cual alimento balanceado')
                                     ->numeric(
                                         decimalPlaces: 1,
                                         decimalSeparator: ',',
                                         thousandsSeparator: '.'
-                                    ),
+                                    )
+                                    ->suffix(' $/Kg')
+                                    ->size('lg'),
 
                                 Infolists\Components\TextEntry::make('consumo_promedio_ms')
-                                    ->label('Consumo promedio MS en terminación (% PV)')
+                                    ->label('Consumo promedio MS en terminación')
                                     ->numeric(
                                         decimalPlaces: 1,
                                         decimalSeparator: ',',
                                         thousandsSeparator: '.'
-                                    ),
+                                    )
+                                    ->suffix(' % PV')
+                                    ->size('lg'),
 
                                 Infolists\Components\TextEntry::make('eficiencia_conversion')
-                                    ->label('Eficiencia conversión (kg MS/kg carne)')
+                                    ->label('Eficiencia conversión')
                                     ->numeric(
                                         decimalPlaces: 1,
                                         decimalSeparator: ',',
                                         thousandsSeparator: '.'
-                                    ),
+                                    )
+                                    ->suffix(' kg MS/kg carne')
+                                    ->size('lg'),
                             ]),
                     ]),
         ]);
