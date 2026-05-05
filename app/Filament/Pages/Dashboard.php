a474fe0f86f5c455c943fedeb33af49d47373392
Mauro <maurogt.dd@gmail.com>
2026-05-05 16:18:43 -0300

    layout escritorio

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Schema;
+
+class Dashboard extends BaseDashboard
+{
+    public function content(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                ...(method_exists($this, 'getFiltersForm') ? [$this->getFiltersFormContentComponent()] : []),
+                Grid::make([
+                    'default' => 1,
+                    'xl' => 2,
+                ])
+                    ->schema([
+                        Grid::make(1)
+                            ->schema($this->getWidgetsSchemaComponents([
+                                \App\Filament\Widgets\ModeloReporteWidget::class,
+                            ])),
+                        Grid::make(1)
+                            ->schema($this->getWidgetsSchemaComponents([
+                                \App\Filament\Widgets\ImpactoCostosWidget::class,
+                                \App\Filament\Widgets\CostoEngordeWidget::class,
+                            ])),
+                    ]),
+            ]);
+    }
+
+    /**
+     * @return int | array<string, ?int>
+     */
+    public function getColumns(): int | array
+    {
+        return [
+            'default' => 1,
+            'xl' => 2,
+        ];
+    }
+
+    /**
+     * @return array<class-string<\Filament\Widgets\Widget> | \Filament\Widgets\WidgetConfiguration>
+     */
+    public function getWidgets(): array
+    {
+        return [
+            \App\Filament\Widgets\ModeloReporteWidget::class,
+            \App\Filament\Widgets\ImpactoCostosWidget::class,
+            \App\Filament\Widgets\CostoEngordeWidget::class,
+        ];
+    }
+}
